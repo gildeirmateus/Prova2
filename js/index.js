33e800a78692aa32324e9a6f8471fa5eed92dfd8
Gildeir Mateus <gildeirmateus.com@gmail.com>
2020-07-09 22:14:37 -0300

    commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+start();
+let list = document.querySelector('input');
+
+function start(){
+    let A1 = document.getElementById('A1');
+    let B1 = document.getElementById('B1');
+    let C1 = document.getElementById('C1');
+    let D1 = document.getElementById('D1');
+    let E1 = document.getElementById('E1');
+    let F1 = document.getElementById('F1');
+
+    let A1 = document.getElementById('A2');
+    let B1 = document.getElementById('B2');
+    let C1 = document.getElementById('C2');
+    let D1 = document.getElementById('D2');
+    let E1 = document.getElementById('E2');
+    let F1 = document.getElementById('F2');
+
+    let A1 = document.getElementById('a3');
+    let B1 = document.getElementById('B3');
+    let C1 = document.getElementById('C3');
+    let D1 = document.getElementById('D3');
+    let E1 = document.getElementById('E3');
+    let F1 = document.getElementById('F3');
+
+    let A1 = document.getElementById('A4');
+    let B1 = document.getElementById('B4');
+    let C1 = document.getElementById('C4');
+    let D1 = document.getElementById('D4');
+    let E1 = document.getElementById('E4');
+    let F1 = document.getElementById('F4');
+    
+    let A1 = document.getElementById('A5');
+    let B1 = document.getElementById('B5');
+    let C1 = document.getElementById('C5');
+    let D1 = document.getElementById('D5');
+    let E1 = document.getElementById('E5');
+    let F1 = document.getElementById('F5');
+
+    addEventListener(list){
+        
+    }
+}
+
+
+
+
